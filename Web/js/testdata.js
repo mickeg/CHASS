b860ec5502d352007645fe1cbe8b06717397db28
Mikael Gerhardsson <clmig@climber.eu>
2017-02-27 22:21:02 +0100

    Testdata

---
+++ Web/js/testdata.js
@@ -0,0 +1,44 @@
+Testdata = [
+{
+    TaxonID : "100155",
+    OBSERVATIONDATUM: {Datum: "2017-01-12", Tid: "12:05:53"},
+    NAMN: "Mindre timmerman",
+    RIKE: "Animalia - djur", 
+    STAM: "Arthropoda - leddjur", 
+    UNDERSTAM: "Hexapoda - insekter",
+    KLASS: "Insecta - egentliga insekter", 
+    UNDERKLASS: "", 
+    ORDNING: "Coleoptera - skalbaggar",
+    UNDERORDNING: "Polyphaga - allätarbaggar",
+    ÖVERFAMILJ: "Chrysomeloidea",  
+    FAMILJ: "Cerambycidae - långhorningar", 
+    UNDERFAMILJ: "Lamiinae",  
+    TRIBUS: "Acanthocinini",  
+    SLÄKTE: "Acanthocinus",  
+    ART: "Acanthocinus griseus(Fabricius, 1792) - mindre timmerman",
+    SYNONYMER: "liten timmerman",
+    Position : {X:123, Y:456}
+},
+{
+    TaxonID : "106665",
+    OBSERVATIONDATUM: {Datum: "2017-01-12", Tid: "12:05:53"},
+    NAMN: "Blåmussla",
+    RIKE: "Mollusca - blötdjur", 
+    STAM: "Arthropoda - leddjur", 
+    UNDERSTAM: "",
+    KLASS: "Bivalvia - musslor", 
+    UNDERKLASS: "Pteriomorphia", 
+    ORDNING: "Mytiloida",
+    UNDERORDNING: "",
+    ÖVERFAMILJ: "",  
+    FAMILJ: "Mytilidae - blåmusslor", 
+    UNDERFAMILJ: "",  
+    TRIBUS: "",  
+    SLÄKTE: "Mytilus",  
+    ART: "Mytilus edulisLinnaeus, 1758 - blåmussla",
+    SYNONYMER: "Mytilus edilis L.",
+    Position : {X:123, Y:456}
+}
+]
+    
+console.log("Testdata: ", Testdata);
