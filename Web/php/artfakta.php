f8fdfabf8f1173dabbb8ddf1446540f1d050b797
Mikael Gerhardsson <clmig@climber.eu>
2017-02-28 20:51:24 +0100

    scrapea artportalen på artbilder

--- Web/php/artfakta.php
+++ Web/php/artfakta.php
@@ -6,24 +6,51 @@
     $doc->preserveWhiteSpace = false;
     $doc->strictErrorChecking = false;
     $doc->recover = true;
-    $t = 106665; //spara denna. (blåmussla)
+    $t = 106665; //blåmussla
 
     $listoftaxons = [6010265, 216215, 100146, 100147, 212363, 102146, 209467, 100149, 100150, 103788, 206118, 215543, 215544, 215957, 215956, 100151, 100152, 106462, 106463, 213487, 100154, 257383, 106370, 106371, 103046, 253680, 105944, 100155];
+    //$listoftaxons = [6010265, 216215, 100146, 100147, 212363, 1021469]; //testa med liten sample size.
 
-    $writeToFile = array();
+    run($listoftaxons);
 
-    createFile();
-    getTaxonDescription($t); //blåmussla
-
-    
-    foreach($listoftaxons as $taxon){
-        echo getTaxonDescription($taxon);
+    function run(){
+        scrapeDescriptionFromArtfakta($listoftaxons);
+        scrapeImagesFromArtportalen($listoftaxons);
     }
 
-    foreach($writeToFile as $line){
-        echo $line;
+    function scrapeImagesFromArtportalen($listoftaxons){
+        foreach($listoftaxons as $taxon){
+            downloadImage($taxon);
+        }
+    }
+
+    function downloadImage($taxon){
+        $url="http://www.artportalen.se/Media/Taxon/".$taxon;
+        $prefix = "http://www.artportalen.se";
+        $html = file_get_contents($url);
+
+        $doc = new DOMDocument();
+        @$doc->loadHTML($html);
+        $tags = $doc->getElementsByTagName('img');
+
+        $i = 1;
+        foreach ($tags as $tag) {
+            $imgpath = $tag->getAttribute('src');
+            $imageurl = $prefix.$imgpath;
+            $dirpath = realpath(dirname(getcwd()));
+            copy($imageurl, $dirpath.'/php/img/'.$taxon.'_'.$i.'.jpg');
+            $i++;
+        }
     }
     
+    function scrapeDescriptionFromArtfakta($listoftaxons){
+        createFile();
+    
+        foreach($listoftaxons as $taxon){
+            $tmp = getTaxonDescription($taxon);
+            writeLineToFile($tmp);
+        }
+    }
     
     function getTaxonDescription($t){
         $GLOBALS['doc']->loadHTMLFile('http://artfakta.artdatabanken.se/taxon/'.$t);
@@ -32,9 +59,8 @@
         $entries = $xpath->query($query);
         $r = $entries->item(0)->textContent;
         $r_utf8 = utf8_decode($r);
-        $result = $r_utf8;//trim(preg_replace('/\s+/', ' ', $r_utf8));  //Tar bort pga. prestanda
-        //writeLineToFile(unicodeString($t) .';'. unicodeString($result));  //Prestanda
-        //echo utf8_decode(unicodeString($t) .';'. unicodeString($result));
+        $result = trim(preg_replace('/\s+/', ' ', $r_utf8)); 
+
         return unicodeString($t) .';'. unicodeString($result);
     }
 
