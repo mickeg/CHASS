547ada2ba062852bd2f14d637984e4b743a27515
Mikael Gerhardsson <clmig@climber.eu>
2017-03-05 21:06:47 +0100

    More testdata

--- Web/node/app.js
+++ Web/node/app.js
@@ -39,7 +39,6 @@ app.get('/imageDirectory', function (req, res) {
 });
 
 
-
 app.get('/csv', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     var inputFile = path.resolve(__dirname + '/Fåglar_2016-08-01_csv_comma.csv');
@@ -72,6 +71,105 @@ app.get('/convert', function (req, res) {
     var t = convertToWGS84(X, Y);
     res.send(t);
 });
+
+
+app.get('/testdata', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    Testdata = [
+    {
+        TaxonID : "100155",
+        NAMN: "Mindre timmerman",
+        Släktskap: {
+            RIKE: "Animalia - djur", 
+            STAM: "Arthropoda - leddjur", 
+            UNDERSTAM: "Hexapoda - insekter",
+            KLASS: "Insecta - egentliga insekter", 
+            UNDERKLASS: "", 
+            ORDNING: "Coleoptera - skalbaggar",
+            UNDERORDNING: "Polyphaga - allätarbaggar",
+            ÖVERFAMILJ: "Chrysomeloidea",  
+            FAMILJ: "Cerambycidae - långhorningar", 
+            UNDERFAMILJ: "Lamiinae",  
+            TRIBUS: "Acanthocinini",  
+            SLÄKTE: "Acanthocinus",  
+            ART: "Acanthocinus griseus(Fabricius, 1792) - mindre timmerman",
+            SYNONYMER: "liten timmerman"
+        },
+        Kännetecken:{
+            Wikipedia: "Mindre timmerman blir 9-12 millimeter lång, möjligen upp till 14 millimeter. Skalbaggen är långsmal, platt och gråspräcklig och har mycket långa antenner, betydligt längre än skalbaggens kropp. Särskilt långa är hanens antenner, de är minst två gånger så långa som kroppen, medan honans antenner är lite kortare, cirka en och en halv gång så långa som kroppen. De enskilda antennsegmenten är tvåfärgade, gråvita mot basen och mörka mot spetsen. Förutom längre antenner har hanen även lite längre ben än honan. Ett annat sätt att se skillnad på hane och hona är att honan vid bakkroppsspetsen har ett långt utstickande äggläggningsrör.",
+            Artfakta: "En påfallande långsmal och platt skalbagge som blir 12–14 mm lång. Honan är försedd med ett långt äggläggningsrör. Antennerna är mycket långa, hos hanen blir de två gånger längre än resten av kroppen. Färgen är svart med en tät grå behåring som på täckvingarna bildar två tvärband."
+        },
+        Observationer: [
+            {Position : {Latitude: 59.980721, Longitude: 17.250436}},
+            {Position : {Latitude: 59.980637, Longitude: 17.250627}},
+            {Position : {Latitude: 59.185407, Longitude: 18.269407}}
+        ]
+    },
+    {
+        TaxonID : "100146",
+        NAMN: "Lövskogslöpare",
+        Släktskap: {
+            RIKE: "Animalia - djur", 
+            STAM: "Arthropoda - leddjur", 
+            UNDERSTAM: "Hexapoda - insekter",
+            KLASS: "Insecta - egentliga insekter", 
+            UNDERKLASS: "", 
+            ORDNING: "Coleoptera - skalbaggar",
+            UNDERORDNING: "Adephaga - rovskalbaggar",
+            ÖVERFAMILJ: "Caraboidea",  
+            FAMILJ: "Carabidae - jordlöpare", 
+            UNDERFAMILJ: "Harpalinae",  
+            TRIBUS: "Pterostichini",  
+            SLÄKTE: "Abax",  
+            ART: "Abax parallelepipedus(Piller & Mitterpacher, 1783) - lövskogslöpare",
+            SYNONYMER: "Abax ater (Villers, 1789)"
+        },
+        Kännetecken:{
+            Wikipedia: "Lövskogslöparen har svarta täckvingar och en bred, nästan fyrkantig svart halssköld. Dess kroppslängd är 18 till 22 millimeter och detta, i kombination med dess kraftiga ben och breda halssköld, ger den ett ganska robust utseende.",
+            Artfakta: "En stor, 18–22 mm, enfärgat svart jordlöpare med relativt robust utseende och kraftiga ben. Täckvingarna är djupt längsfårade och halsskölden är påfallande bred och nästan kvadratisk, vilket ger djuret en parallellsidig kroppsform."
+        },
+        Observationer: [
+            {Position : {Latitude: 57.439673, Longitude: 12.194366}}
+        ]
+    },
+
+    {
+        TaxonID : "102004",
+        NAMN: "Ekgetingbock",
+        Släktskap: {
+            RIKE: "Animalia - djur", 
+            STAM: "Arthropoda - leddjur", 
+            UNDERSTAM: "Hexapoda - insekter",
+            KLASS: "Insecta - egentliga insekter", 
+            UNDERKLASS: "", 
+            ORDNING: "Coleoptera - skalbaggar",
+            UNDERORDNING: "Adephaga - allätarbaggar",
+            ÖVERFAMILJ: "Chrysomeloidea",  
+            FAMILJ: "Cerambycidae - långhorningar", 
+            UNDERFAMILJ: "Cerambycinae",  
+            TRIBUS: "Clytini",  
+            SLÄKTE: "Xylotrechus",  
+            ART: "Xylotrechus antilope(Schönherr, 1817) - ekgetingbock",
+            SYNONYMER: "smal getingbock"
+        },
+        Kännetecken:{
+            Wikipedia: "",
+            Artfakta: "En svart, cylindriskt byggd art som blir 8–14 mm lång. På halsskölden och täckvingarna finns tydliga gulvita hårfläckar. På täckvingarna finns dessutom två tydliga, delvis snedställda tvärband. Färgteckningen ger arten ett utseende som liknar en geting och bör vara ett tydligt exempel på s.k. Bates mimikry. Benen och antenner är gulbruna. Benen är påfallande långa, speciellt barkbenen."
+        },
+        Observationer: [
+            {Position : {Latitude: 56.969171, Longitude: 16.077839}},
+            {Position : {Latitude: 56.934637, Longitude: 16.027558}},
+            {Position : {Latitude: 57.005427, Longitude: 16.219369}}
+        ]
+    }
+
+    ]
+
+    res.send(Testdata);
+});
+
+
 
 
 
